a7e099c3af467f6deace25fd1607da6a78c38461
Robson Magno Ramos das Neves <robsonmagno@MacBook-Pro-de-Robson.local>
2019-05-28 15:57:31 -0400

    Conceito de DefaultProps

---
+++ src/components/Todo.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+
+import { View, Text, StyleSheet } from 'react-native';
+
+export default class Todo extends Component {
+  static defaultProps = {
+    title: 'Todo Padrão',
+  }
+
+  render() {
+    return (
+      <View>
+        <Text>{this.props.title}</Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+
+});
--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import { Platform, StyleSheet, Text, View } from 'react-native';
+import Todo from './components/Todo';
 
 export default class App extends Component {
   render() {
     return (
       <View style={styles.container}>
-
+        <Todo title="Fazer cafe" />
+        <Todo />
       </View>
     );
   }
