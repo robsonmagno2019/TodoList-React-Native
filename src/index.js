60375d0858b2851b19f7d7a54a8825ec1d05c4f0
Robson Magno Ramos das Neves <robsonmagno@MacBook-Pro-de-Robson.local>
2019-05-28 16:35:57 -0400

    Imutabilidade

--- src/index.js
+++ src/index.js
@@ -1,13 +1,28 @@
 import React, { Component } from 'react';
-import { Platform, StyleSheet, Text, View } from 'react-native';
+import { Platform, StyleSheet, Text, View, Button } from 'react-native';
 import Todo from './components/Todo';
 
 export default class App extends Component {
+  state = {
+    usuario: 'Robson',
+    todos: [
+      { id: 0, text: 'Fazer café' },
+      { id: 1, text: 'Estudar o GoNative' },
+    ],
+  }
+
+  addTodo = () => {
+    this.setState({
+      todos: [...this.state.todos, { id: Math.random(), text: 'Novo Todo' }]
+    });
+  }
+
   render() {
     return (
       <View style={styles.container}>
-        <Todo title="Fazer cafe" />
-        <Todo />
+        <Text>{this.state.usuario}</Text>
+        {this.state.todos.map(todo => <Todo key={todo.id} title={todo.text} />)}
+        <Button title="Adicionar Todo" onPress={this.addTodo} />
       </View>
     );
   }
